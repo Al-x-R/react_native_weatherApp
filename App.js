3c86a470194122e970cc93edb2d9f717f0970ecf
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-03-22 14:53:42 +0200

    Add simple loading component

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native-web';
+import { StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Loading from './Loading';
+import WeatherDataComponent from './WeatherDataComponent';
+
+export default class extends React.Component {
+		constructor(props) {
+				super(props);
+				this.state = {
+						isLoading: true,
+						latitude: '',
+						longitude: ''
+				};
+		}
+
+		getCurrentLocation = async () => {
+				try {
+						await Location.requestPermissionsAsync();
+						const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({});
+
+						if (latitude && longitude) {
+								this.setState({
+										isLoading: false,
+										latitude,
+										longitude
+								});
+						}
+
+				} catch (e) {
+						Alert.alert('Permission to access location was denied', '');
+				}
+		};
+
+		componentDidMount() {
+				this.getCurrentLocation();
+		}
+
+		render() {
+				const { isLoading, latitude, longitude } = this.state;
+
+				return (
+						isLoading ? <Loading/> : <WeatherDataComponent latitude={latitude} longitude={longitude}/>
+				);
+		}
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+		container: {
+				flex: 1,
+				backgroundColor: '#fff',
+				alignItems: 'center',
+				justifyContent: 'center',
+		},
 });
